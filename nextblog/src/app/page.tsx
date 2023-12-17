a8908587dbbbe061fabfdddaac591d8a9d7e223f
surya kant <suryakant0013@yahoo.in>
2023-12-17 21:55:53 +0530

    added hero to Home

--- nextblog/src/app/page.tsx
+++ nextblog/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import Navbar from '@/components/Navbar'
+import Hero from '@/components/Hero'
 import { memo } from 'react'
 
 export default function Home() {
   return (
+    <>
     <Navbar/>
+    <Hero/>
+    </>
       )
 }
--- nextblog/src/components/Hero.tsx
+++ nextblog/src/components/Hero.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
 const Hero = () => {
   return (
     <div className="container mx-auto">
-      <div className="flex flex-wrap -mx-4">
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
