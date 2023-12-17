40989b71e48555fc8fe9e99acd7ec876bc8b4c3d
surya kant <suryakant0013@yahoo.in>
2023-12-17 20:38:53 +0530

    added demo card and hero

---
+++ nextblog/src/components/Hero.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card from './Card'
+
+const Hero = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-wrap -mx-4">
+       <Card/>
+       <Card/>
+       <Card/>
+       <Card/>
+       <Card/>
+       <Card/>
+       <Card/>
+       <Card/>
+       <Card/>
+       <Card/>
+       <Card/>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
