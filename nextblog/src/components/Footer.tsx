31d8e1d8124fa396f6e3dd9ad27162995363a6a0
surya kant <suryakant0013@yahoo.in>
2023-12-18 13:25:18 +0530

    added demo footer

---
+++ nextblog/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="join">
+      <button className="join-item btn">1</button>
+      <button className="join-item btn">2</button>
+      <button className="join-item btn btn-disabled">...</button>
+      <button className="join-item btn">99</button>
+      <button className="join-item btn">100</button>
+    </div>
+  );
+};
+
+export default Footer;
