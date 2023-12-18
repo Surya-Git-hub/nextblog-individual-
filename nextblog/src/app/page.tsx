75ed6f496ead202d426145a528e1d5d1745cd001
surya kant <suryakant0013@yahoo.in>
2023-12-18 15:27:58 +0530

    centered footer

--- nextblog/src/app/page.tsx
+++ nextblog/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
+import Footer from '@/components/Footer'
 import { memo } from 'react'
 
 export default function Home() {
--- nextblog/src/components/Footer.tsx
+++ nextblog/src/components/Footer.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="join">
-      <button className="join-item btn">1</button>
-      <button className="join-item btn">2</button>
-      <button className="join-item btn btn-disabled">...</button>
-      <button className="join-item btn">99</button>
-      <button className="join-item btn">100</button>
+    <div className="flex justify-center mt-4	">
+      <div className="join">
+        <button className="join-item btn">1</button>
+        <button className="join-item btn">2</button>
+        <button className="join-item btn btn-disabled">...</button>
+        <button className="join-item btn">99</button>
+        <button className="join-item btn">100</button>
+      </div>
     </div>
   );
 };
