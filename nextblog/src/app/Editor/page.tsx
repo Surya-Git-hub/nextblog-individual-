a83994c4ab8c050281cc26e477831437e1d6dc6c
surya kant <suryakant0013@yahoo.in>
2023-12-22 00:54:03 +0530

    added novel editor

---
+++ nextblog/src/app/Editor/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Beditor from '@/components/Beditor'
+
+const page = () => {
+  return (
+    <Beditor/>
+  )
+}
+
+export default page
---
+++ nextblog/src/components/Beditor.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Editor } from 'novel'
+
+const Beditor = () => {
+  return (
+    <Editor/>
+  )
+}
+
+export default Beditor
