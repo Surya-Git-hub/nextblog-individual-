158b51710ded3db34265ca929f1fccb489fac990
surya kant <suryakant0013@yahoo.in>
2023-12-23 23:42:33 +0530

    added rendrer to create page

--- nextblog/src/app/blog/create/page.tsx
+++ nextblog/src/app/blog/create/page.tsx
@@ -3,17 +3,33 @@ import { OutputData } from "@editorjs/editorjs";
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import { useState } from "react";
+import EditorJsRenderer from "@/components/EditorJsRenderer";
 
 const EditorBlock = dynamic(() => import("@/components/Editor"), {
   ssr: false,
 });
 
-const page:NextPage = () => {
+const page: NextPage = () => {
   const [data, setData] = useState<OutputData>();
 
   return (
-    <div className="container max-w-4xl">
-    <EditorBlock data={data} onChange={setData} holder="editorjs-container" />
+    <div className="grid grid-cols-2 gap-2">
+      <div className="col-span-1 ">
+        <h1>Editor</h1>
+        <div className="border rounded-md">
+          <EditorBlock
+            data={data}
+            onChange={setData}
+            holder="editorjs-container"
+          />
+        </div>
+      </div>
+      <div className="col-span-1 ">
+        <h1>Preview</h1>
+        <div className="border rounded-md">
+          <div className="p-16">{data && <EditorJsRenderer data={data} />}</div>
+        </div>
+      </div>
     </div>
   );
 };
