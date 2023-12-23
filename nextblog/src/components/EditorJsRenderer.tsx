1be08ad6744d03c9ff426c3d7dde9cdb7a3606fd
surya kant <suryakant0013@yahoo.in>
2023-12-23 23:29:27 +0530

    completed rendrer component

--- nextblog/src/components/EditorJsRenderer.tsx
+++ nextblog/src/components/EditorJsRenderer.tsx
@@ -1,3 +1,30 @@
-
 import { OutputData } from "@editorjs/editorjs";
 import React from "react";
+
+const editorjsHtml = require("editorjs-html");
+const EditorjsToHtml = editorjsHtml();
+
+type Props = {
+  data: OutputData;
+};
+
+type ParsedContent = string | JSX.Element;
+
+const EditorJsRenderer = ({ data }: Props) => {
+  const html = EditorjsToHtml.parse(data) as ParsedContent[];
+
+  return (
+    <div className="prose max-w-full" key={data.time}>
+      {html.map((item, index) => {
+        if (typeof item === "string") {
+          return (
+            <div dangerouslySetInnerHTML={{ __html: item }} key={index}></div>
+          );
+        }
+        return item;
+      })}
+    </div>
+  );
+};
+
+export default EditorJsRenderer;
