1439d045dbea02fbbafa93c4509298985381dd0e
surya kant <suryakant0013@yahoo.in>
2023-12-25 20:07:15 +0530

    added novel editor style config

--- nextblog/src/app/blog/create2/page.tsx
+++ nextblog/src/app/blog/create2/page.tsx
@@ -1,9 +1,20 @@
-import Tiptap from "@/components/Tiptap";
-
+import { Editor } from "novel";
 import React from "react";
 
 const page = () => {
-  return <Tiptap />;
+  return (
+    <div>
+      <Editor
+        className="prose max-w-full"
+        editorProps={{
+          attributes: {
+            class:
+              "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none",
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default page;
