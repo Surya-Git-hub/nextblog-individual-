71535c46df5d28e6d9e562a4b14183af288776b6
surya kant <suryakant0013@yahoo.in>
2023-12-28 02:09:55 +0530

    added editor in slug and create page

--- nextblog/src/app/blog/[slug]/page.tsx
+++ nextblog/src/app/blog/[slug]/page.tsx
@@ -1,20 +1,42 @@
 "use client";
 import React, { useMemo } from "react";
-import { generateHTML } from "@tiptap/react";
-import { defaultExtensions } from "@/lib/default-extensions";
+import dynamic from "next/dynamic";
+const Editor = dynamic(() => import("@/components/Editor"), { ssr: false });
+// import { generateHTML } from "@tiptap/react";
+// import { defaultExtensions } from "@/lib/default-extensions";
 // import UploadImagesPlugin from "@/ui/editor/plugins/upload-images";
 // import UpdatedImage from "./updated-image";
+// import { BlockNoteEditor } from "@blocknote/core";
+// import {
+//   BlockNoteView,
+//   lightDefaultTheme,
+//   useBlockNote,
+// } from "@blocknote/react";
+// import "@blocknote/core/style.css";
 
 const page = () => {
-  let json: any = localStorage.getItem("novel__content");
-  json = JSON.parse(json);
-  console.log("first", json);
-  const output = useMemo(() => {
-    return generateHTML(json, defaultExtensions);
-  }, [json]);
+  // let json: any;
+  // if (typeof window !== "undefined" && window.localStorage) {
+  //   json = localStorage.getItem("editor");
+  //   json = JSON.parse(json);
+  //   console.log("first", json);
+  // }
+
+  // const editor: BlockNoteEditor | null = useBlockNote({
+  //   editable: false,
+  //   // initialContent: json,
+  // });
+
+  let json: any;
+  if (typeof window !== "undefined" && window.localStorage) {
+    json = localStorage.getItem("editor");
+    json = JSON.parse(json);
+    // console.log("first", json);
+  }
   return (
     <div className=" flex flex-col items-center p-24 w-full">
-      <div className="prose" dangerouslySetInnerHTML={{ __html: output }}></div>
+      <Editor content={json} isEditable={false} />
+      {/* <BlockNoteView editor={editor} theme={lightDefaultTheme} /> */}
     </div>
   );
 };
--- nextblog/src/app/blog/create/page.tsx
+++ nextblog/src/app/blog/create/page.tsx
@@ -1,11 +1,23 @@
-import { Editor } from "novel";
+"use client";
+// import { Editor, } from "novel";
+import dynamic from "next/dynamic";
+const Editor = dynamic(() => import("@/components/Editor"), { ssr: false });
+
+// import {generateHTML} from "@tiptap/html";
 import React from "react";
 
 const page = () => {
+  let json:any 
+  if (typeof window !== 'undefined' && window.localStorage) {
+    
+    json = localStorage.getItem("editor");
+    json = JSON.parse(json);
+    // console.log("first", json);
+
+    }
   return (
-    <div className=" flex min-h-screen flex-col items-center justify-between p-24">
-      <Editor 
-      />
+    <div className=" flex flex-col items-center p-24 w-full">
+      <Editor content={json} isEditable={true}/>
     </div>
   );
 };
