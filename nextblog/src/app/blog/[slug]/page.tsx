4006c388da1ea7cef1106bcdc0a80de33f29deef
surya kant <suryakant0013@yahoo.in>
2023-12-27 12:52:46 +0530

    added default extension export and slug page renderer

--- nextblog/src/app/blog/[slug]/page.tsx
+++ nextblog/src/app/blog/[slug]/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+import React, { useMemo } from "react";
+import { generateHTML } from "@tiptap/react";
+import { defaultExtensions } from "@/lib/default-extensions";
+// import UploadImagesPlugin from "@/ui/editor/plugins/upload-images";
+// import UpdatedImage from "./updated-image";
 
 const page = () => {
+  let json: any = localStorage.getItem("novel__content");
+  json = JSON.parse(json);
+  console.log("first", json);
+  const output = useMemo(() => {
+    return generateHTML(json, defaultExtensions);
+  }, [json]);
   return (
-    <div>page</div>
-  )
-}
+    <div className=" flex flex-col items-center p-24 w-full">
+      <div className="prose" dangerouslySetInnerHTML={{ __html: output }}></div>
+    </div>
+  );
+};
 
-export default page
+export default page;
