a919a97e8e51a3feaf60a0cfd4aeb9140d19cb07
surya kant <suryakant0013@yahoo.in>
2023-12-30 01:12:43 +0530

    added options file next providers

---
+++ nextblog/src/app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,48 @@
+import Github, { GithubProfile } from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+
+export const options = {
+    providers: [
+        Github({
+            profile(profile) {
+                console.log("Profile Github: ", profile)
+
+                let userRole = "Github User"
+                if (profile?.email == "codesorcerers@gmail.com") {
+                    userRole = "admin"
+                }
+                return {
+                    ...profile,
+                    role: userRole
+                }
+            },
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+        }),
+        Google({
+            profile(profile) {
+                console.log("Profile Google: ", profile)
+
+
+                return {
+                    ...profile,
+                    id: profile.sub,
+                    role: userRole
+                }
+            },
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
+        })
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) token.role = user.role;
+            return token
+        },
+        async session({ session, token }) {
+            if (session?.user) session.user.role = token.role;
+            return session;
+        }
+    }
+}
