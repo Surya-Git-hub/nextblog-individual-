b975c7429ee34d548831d6d28de199b698c43d2e
surya kant <suryakant0013@yahoo.in>
2024-01-01 13:51:18 +0530

    code refactor and navbar client session added

--- nextblog/src/components/Navbar.tsx
+++ nextblog/src/components/Navbar.tsx
@@ -2,8 +2,24 @@
 
 import useTheme from "@/context/theme";
 import React, { useEffect, useState } from "react";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
+  // let session;
+  // (async () => {
+  //   session = await getServerSession(options);
+  // })();
+const {data:session}=useSession({
+  required:false,
+  onUnauthenticated(){
+    console.log("unauthenticated")
+  }
+});
+
+console.log("session",session)
   const { themeMode, darkTheme, lightTheme } = useTheme();
   const [isChecked, setChecked] = useState(false);
 
@@ -31,7 +47,7 @@ const Navbar = () => {
   useEffect(() => {
     handleCheckboxChange();
   }, []);
-  
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -73,6 +89,11 @@ const Navbar = () => {
             <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
           </svg>
         </label>
+        {session ? (
+          <Link href={"/api/auth/signout?callbackUrl=/"}>Logout</Link>
+        ) : (
+          <Link href={"/api/auth/signin"}>Login</Link>
+        )}
         <div className="dropdown dropdown-end">
           <div
             tabIndex={0}
@@ -82,7 +103,7 @@ const Navbar = () => {
             <div className="w-10 rounded-full">
               <img
                 alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                src={session?(session?.user?.image):("https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg")}
               />
             </div>
           </div>
