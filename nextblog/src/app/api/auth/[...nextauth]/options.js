d846ee73308083be395aee53a02d8f0c616a1bf0
surya kant <suryakant0013@yahoo.in>
2024-01-02 23:48:03 +0530

    added credentials provider

--- nextblog/src/app/api/auth/[...nextauth]/options.js
+++ nextblog/src/app/api/auth/[...nextauth]/options.js
@@ -1,5 +1,6 @@
 import Github, { GithubProfile } from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials"
 
 
 export const options = {
@@ -15,7 +16,7 @@ export const options = {
                 return {
                     ...profile,
                     role: userRole,
-                    image:profile.avatar_url
+                    image: profile.avatar_url
                 }
             },
             clientId: process.env.GITHUB_ID,
@@ -34,6 +35,44 @@ export const options = {
             },
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET
+        }),
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. 'Sign in with...')
+            name: 'nextBlog',
+            // The credentials is used to generate a suitable form on the sign in page.
+            // You can specify whatever fields you are expecting to be submitted.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                email: {
+                    label: "email:",
+                    type: "text",
+                    placeholder: "your-email",
+                  },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // You need to provide your own logic here that takes the credentials
+                // submitted and returns either a object representing a user or value
+                // that is false/null if the credentials are invalid.
+                // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+                // You can also use the `req` object to obtain additional parameters
+                // (i.e., the request IP address)
+                const res = await fetch("/your/endpoint", {
+                    method: 'POST',
+                    body: JSON.stringify(credentials),
+                    headers: { "Content-Type": "application/json" }
+                })
+                const user = await res.json()
+
+                // If no error and we have user data, return it
+                if (res.ok && user) {
+                    return user
+                }
+                // Return null if user data could not be retrieved
+                return null
+            }
         })
     ],
     callbacks: {
