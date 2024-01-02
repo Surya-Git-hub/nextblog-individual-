4ba12ef908fc65331c1f35a742b72925dfe2f4e2
surya kant <suryakant0013@yahoo.in>
2024-01-02 23:52:10 +0530

    seversession in server component

--- nextblog/src/app/serverMember/page.tsx
+++ nextblog/src/app/serverMember/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { getServerSession } from "next-auth";
+import React from "react";
+import { options } from "../api/auth/[...nextauth]/options";
 
-const page = () => {
+const page = async () => {
+  const session = await getServerSession(options);
   return (
     <div>
-        <h1>Member Server Session</h1>
+      <h1>Member Server Session</h1>
+      <div>
+        user name === {session?.user} 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
