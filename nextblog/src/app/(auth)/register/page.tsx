1f591e767d78ca4cc06603f688474baa586fb071
surya kant <suryakant0013@yahoo.in>
2024-01-06 00:14:26 +0530

    created signin page for next auth

--- nextblog/src/app/(auth)/register/page.tsx
+++ nextblog/src/app/(auth)/register/page.tsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
     <div>
-        <h1>MEMBER server Session</h1>
-    </div>
-  )
-}
+      <div className="relative flex flex-col items-center justify-center h-screen overflow-hidden">
+        <div className="w-full p-6 border-t-4  rounded-md shadow-md border-top lg:max-w-lg">
+          <h1 className="text-3xl font-semibold text-center">
+            Login 
+          </h1>
+          <form className="space-y-4">
+            <div>
+              <label className="label">
+                <span className="text-base label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Email Address"
+                className="w-full input input-bordered"
+              />
+            </div>
+            <div>
+              <label className="label">
+                <span className="text-base label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Enter Password"
+                className="w-full input input-bordered"
+              />
+            </div>
+            <a
+              href="#"
+              className="text-xs  hover:underline hover:text-blue-600"
+            >
+              Forget Password?
+            </a>
+            <br/>
+            <a
+              href="#"
+              className="text-xs  hover:underline hover:text-blue-600"
+            >
+              New User Register Instead?
+            </a>
+            <div>
+              <button className="btn btn-block">Login</button>
+            </div>
+            <div className="flex justify-around ">
+            <button className="btn  glass ">Login with Google</button>
+            <button className="btn  glass">Login with Github</button>
+            </div>
 
-export default page
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
